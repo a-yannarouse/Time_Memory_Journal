86d2b664e0375d881900b37e4a5a248bb45634c5
NawafGH <Nawafkg@bu.edu>
2023-11-09 20:22:46 -0500

    added login/signup feature in backend

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Travel Journal API');
 });
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
